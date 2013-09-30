8ec61071906276bf28cde187ec0657fcf1b78eb5
Jeroen Jacobs <git@jeroenj.be>
2013-09-30 10:58:06 +0200

    Using inverse_of

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ActiveRecord::Base
   extend FriendlyId
-  
+
   friendly_id :name, use: :slugged
-  belongs_to :person
+  belongs_to :person, inverse_of: :accounts
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: [:slugged, :history]
 
-  has_many :accounts
+  has_many :accounts, inverse_of: :person
 
   def should_generate_new_friendly_id?
     slug.blank? || name_changed?
